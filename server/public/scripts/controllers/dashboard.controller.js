ecf2c45fbca22ac9a7b70a7002e07c0cc07d73d7
Paia Thao <paiathao09@gmail.com>
2018-07-21 15:54:11 -0500

    edit button added

--- server/public/scripts/controllers/dashboard.controller.js
+++ server/public/scripts/controllers/dashboard.controller.js
@@ -1,43 +1,55 @@
 app.controller('DashboardController', ['HotelService', function (HotelService) {
-    console.log('Dashboard Controller loaded')
-    let self = this;
+  console.log('Dashboard Controller loaded')
+  let self = this;
 
-    self.pets = HotelService.pets;
-    self.owners = HotelService.owners;
+  self.pets = HotelService.pets;
+  self.owners = HotelService.owners;
 
-    self.getAllOwners = HotelService.getAllOwners;
+  self.getAllOwners = HotelService.getAllOwners;
 
-    self.getAllPets = HotelService.getAllPets;
+  self.getAllPets = HotelService.getAllPets;
 
-    self.addPet = function (newPet) {
-      HotelService.addPet(newPet);
-    }
+  self.addPet = function (newPet) {
+    HotelService.addPet(newPet);
+  }
 
-    self.updatePetCheckIn = function (pet) {
-      HotelService.updatePetCheckIn(pet)
-    }
+  self.updatePetCheckIn = function (pet) {
+    HotelService.updatePetCheckIn(pet)
+  }
 
-    self.deletePet = function (petId) {
-      swal({
-        title: 'Are you sure you want to delete this pet?',
-        text: 'Once you\'ve deleted, this pet will be gone!',
-        icon: 'warning',
-        buttons: true,
-        dangerMode: true,
+  self.deletePet = function (petId) {
+    swal({
+      title: 'Are you sure you want to delete this pet?',
+      text: 'Once you\'ve deleted, this pet will be gone!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          HotelService.deletePet(petId);
+          swal('Pet has been deleted', {
+            icon: 'success',
+          });
+        } else {
+          swal('Request has been cancelled!');
+        }
       })
-        .then((willDelete) => {
-          if (willDelete) {
-            HotelService.deletePet(petId);
-            swal('Pet has been deleted', {
-              icon: 'success',
-            });
-          } else {
-            swal('Request has been cancelled!');
-          }
-        })
-      
-    }
-  
 
-      
-  }]);
+  }
+
+  // self.editPet = function (petToEdit) {
+  //   console.log('pet to edit', petToEdit)
+  //   self.editing = true;
+  //   self.editing_id = petToEdit.pet_id;
+  //   //put the pet info into existing inputs
+  //   self.newPet.name = petToEdit.pet_name;
+  //   self.newPet.breed = petToEdit.pet_breed;
+  //   self.newPet.color = petToEdit.pet_color;
+  //   self.newPet.owner_id = petToEdit.owner_id;
+  //   // console.log(self.newPet)
+  // }
+
+
+
+}]);
